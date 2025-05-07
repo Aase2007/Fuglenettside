0aecadbf1b9a4de836d86901a98808d8579b2166
Aase2007 <ase.luras@gmail.com>
2025-05-07 11:37:59 +0200

    mariadb fuglerliste i frontenden

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,5 @@
 const API_URL = 'http://localhost:3000/'
-let cors = require("cors")
-app.use(cors())
-let fuglerListe
+let fuglerListe = []
 
 async function getObservasjoner() {
   const res = await fetch(API_URL + "observasjoner");
@@ -19,27 +17,46 @@ async function getObservasjoner() {
 }
 getObservasjoner()
 
-async function fetchCSV() {
-  try {
-      const response = await fetch('frontend/fuglerliste.csv');
-      const data = await response.text();
-      const list = await data.split("\r\n")
-      console.log(list)
-      return list
-  } catch (error) {
-      console.error('Error fetching CSV:', error);
+async function getFugler() {
+  const res = await fetch(API_URL + "fugler")
+  const data = await res.json();
+  return data
+}
+getFugler().then((result) => {
+  for(let i=0; i<result.length; i++){
+    fuglerListe.push(result[i].nameNO)
   }
-};
-
-fetchCSV().then((result) => {
-  fuglerListe = result
   fuglerListe.sort((a, b) => a.length - b.length);
   for (let element = 0; element < fuglerListe.length; element++ ) {
     let listeelement = document.createElement('div')
     listeelement.innerHTML = `<li onclick="clickItem(${element})" role="presentation" class="link"> ${fuglerListe[element]} </li>`
     document.getElementById('autofill').appendChild(listeelement)
   }
+  console.log(fuglerListe)
 }).catch(console.error);
+
+
+// async function fetchCSV() {
+//   try {
+//       const response = await fetch('frontend/fuglerliste.csv');
+//       const data = await response.text();
+//       const list = await data.split("\r\n")
+//       console.log(list)
+//       return list
+//   } catch (error) {
+//       console.error('Error fetching CSV:', error);
+//   }
+// };
+
+// fetchCSV().then((result) => {
+//   fuglerListe = result
+//   fuglerListe.sort((a, b) => a.length - b.length);
+//   for (let element = 0; element < fuglerListe.length; element++ ) {
+//     let listeelement = document.createElement('div')
+//     listeelement.innerHTML = `<li onclick="clickItem(${element})" role="presentation" class="link"> ${fuglerListe[element]} </li>`
+//     document.getElementById('autofill').appendChild(listeelement)
+//   }
+// }).catch(console.error);
 
 
 function search() {
